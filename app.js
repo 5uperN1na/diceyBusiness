d73618d45d0aa53598cedbec9dd870c332c7178f
Paula Suarez <Paula.Suarez@freeman.com>
2019-05-03 12:49:52 -0500

    Adding updates to files in repo.

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
 //create global array
-let objArray = [];
+let array = [];
 
 //Function to generate random number.
-function random(){
+function random() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-let Dice = function(){
-    this.value = this.roll();
+//Dice function
+let Dice = function () {
+    //Call roll dice function and assign value.
+    this.value = this.rollDice();
+    //Create the div.
     this.div = document.createElement('div');
+    //Assign previous value to div.
     this.div.innerText = this.value;
+    //Assign a classname.
     this.div.className = 'die';
-    document.getElementById('container').appendChild(this.div);
-    this.div.addEventListener('click', function(){
-        this.value = this.roll();
-        this.div.innerText = this.value;
-    }.bind(this));
-    this.div.addEventListener('dblclick', function(){
+    //Append div to the div box from the HTML.
+    document.getElementById('box').appendChild(this.div);
+
+    //Add an event listener when double click, it deletes.
+    //Remove the value from the array as well.
+    this.div.addEventListener('dblclick', function () {
         this.div.remove();
-         let id = objArray.indexOf(this);
-        objArray.splice(id,1);
+        let id = array.indexOf(this);
+        array.splice(id, 1);
     }.bind(this))
+
 }
 
 //Associate random number function when die is rolled.
-Dice.prototype.roll = function() {
+Dice.prototype.rollDice = function () {
     return random();
 }
 
 //Get generate button from html and add an event listener to it.
-//Create a function to get value rolled and push value onto global array already declared.
-document.getElementById('generate').addEventListener('click', function(){
+//Call the Dice function to get value rolled and push value onto global array already declared.
+document.getElementById('generate').addEventListener('click', function () {
     let value = new Dice();
-    objArray.push(value);
+    array.push(value);
 })
 
 //Get roll button from html and add an event listener to it.
-//Create a function with a for loop to
-document.getElementById('roll').addEventListener('click', function(){
-
-
-
-
-
-        for(let i = 0; i < objArray.length; i++){
-            objArray[i].value = objArray[i].roll();
-            objArray[i].div.innerText = objArray[i].value;
-        }
+//Create a for loop to loop through array values and call rollDice function to generate new values.
+document.getElementById('rolling').addEventListener('click', function () {
+    for (let j = 0; j < array.length; j++) {
+        array[j].value = array[j].rollDice();
+        array[j].div.innerText = array[j].value;
+    }
 })
 
-document.getElementById('sum').addEventListener('click', function(){
+//Get sum button from html and add an event listener to it.
+//Create a for loop to loop through each value currently displayed (and store in the array) and sum for total.
+
+document.getElementById('sum').addEventListener('click', function () {
     let total = 0;
-    for (let i = 0; i < objArray.length; i++){
-        total += objArray[i].value;
+    for (let k = 0; k < array.length; k++) {
+        total += array[k].value;
     }
-    alert('The sum is' + total);
+    alert('The total value of each die is: ' + total);
 })
